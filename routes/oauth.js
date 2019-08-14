e3c243d6d08af2a0f56d69c24002b9665efdb09f
Yahya EZ-ZAINABI <yez-zain@student.1337.ma>
2019-08-14 05:34:45 +0000

    rename oauthMiddleware and add renderMiddleware

--- routes/oauth.js
+++ routes/oauth.js
@@ -2,8 +2,12 @@ const passport = require('passport')
 const express = require('express')
 const router = express.Router()
 
-const oauthMiddleware = (err, req, res, next) => {
+const errorMiddleware = (err, req, res, next) => {
 	if (err) return res.redirect('/')
+}
+
+const renderMiddleware = (req, res) => {
+	res.render('redirect', { token: req.user.addToken().token })
 }
 
 router.get('/google', passport.authenticate('google', {
@@ -13,32 +17,22 @@ router.get('/google', passport.authenticate('google', {
 	]
 }))
 
-router.get('/googlered', passport.authenticate('google'), oauthMiddleware, (req, res) => {
-	res.render('redirect', { token: req.user.addToken().token })
-})
+router.get('/googlered', passport.authenticate('google'), errorMiddleware, renderMiddleware)
 
 router.get('/ft', passport.authenticate('42'))
 
-router.get('/ft_ret', passport.authenticate('42'), oauthMiddleware, (req, res) => {
-	res.render('redirect', { token: req.user.addToken().token })
-})
+router.get('/ft_ret', passport.authenticate('42'), errorMiddleware, renderMiddleware)
 
 router.get('/fb', passport.authenticate('facebook', {scope:['email']}))
 
-router.get('/fb_ret', passport.authenticate('facebook'), oauthMiddleware, (req, res) => {
-	res.render('redirect', { token: req.user.addToken().token })
-})
+router.get('/fb_ret', passport.authenticate('facebook'), errorMiddleware, renderMiddleware)
 
 router.get('/li', passport.authenticate('linkedin'))
 
-router.get('/li_ret', passport.authenticate('linkedin'), oauthMiddleware, (req, res) => {
-	res.render('redirect', { token: req.user.addToken().token })
-})
+router.get('/li_ret', passport.authenticate('linkedin'), errorMiddleware, renderMiddleware)
 
 router.get('/git', passport.authenticate('github'))
 
-router.get('/git_ret', passport.authenticate('github'), oauthMiddleware, (req, res) => {
-	res.render('redirect', { token: req.user.addToken().token })
-})
+router.get('/git_ret', passport.authenticate('github'), errorMiddleware, renderMiddleware)
 
 module.exports = router
