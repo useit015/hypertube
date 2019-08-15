e0c6b5b72648970db557c7e2688134cd08f8e002
Yahya EZ-ZAINABI <yez-zain@student.1337.ma>
2019-08-15 12:42:05 +0000

    Move signing in new page

---
+++ client/pages/signing.vue
@@ -0,0 +1,39 @@
+<template>
+	<v-layout column>
+		<v-flex xs12 sm7 md6 lg5 xl3 class="my-5">
+			<v-layout justify-end align-center>
+				<v-btn-toggle v-model="login" mandatory rounded class="toggle">
+					<v-btn class="pl-5 pr-3" color="primary" outlined>Sign up</v-btn>
+					<v-btn class="pr-5 pl-3" color="primary" outlined>Sign in</v-btn>
+				</v-btn-toggle>
+			</v-layout>
+		</v-flex>
+		<login v-if="login"/>
+		<register v-else/>
+		<div class="py-3 mb-3 headline font-weight-thin text-center">OR</div>
+		<strategy/>
+	</v-layout>
+</template>
+
+<script>
+	import Login from "@/components/login";
+	import Register from "@/components/register";
+	import Strategy from "@/components/strategy";
+
+	export default {
+		components: {
+			Login,
+			Register,
+			Strategy
+		},
+		data: () => ({
+			login: 1
+		})
+	};
+</script>
+
+<style>
+.toggle {
+	overflow: hidden;
+}
+</style>
