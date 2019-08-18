fb2c2db17c3b6299adc458110720aa60477bca94
Yahya EZ-ZAINABI <yezzainabi@gmail.com>
2019-08-18 18:58:43 +0100

    add loader to profile

--- client/pages/profile/_username.vue
+++ client/pages/profile/_username.vue
@@ -1,21 +1,24 @@
 <template>
-<div class="user">
+<div v-if="loaded" class="user">
     <div v-if="user">
         <h1>{{user.firstName}}</h1>
         <h1>{{user.lastName}}</h1>
         <h1>{{user.username}}</h1>
+        <img :src="user.image" />
     </div>
     <div v-else>
         <h1>User not found</h1>
     </div>
 </div>
+<loader v-else/>
 </template>
 
 <script>
     import axios from "axios";
-
+    import loader from '@/components/loader';
     const fetchUser = async (username) => {
-        const res = await axios.get(`https://hypertube.tk/api/users/user/${username}`)
+        const token = localStorage.getItem('token')
+        const res = await axios.get(`https://hypertube.tk/api/users/user/${username}`, {headers:{Authorization:`jwt ${token}`}})
         if (!res.data.err) {
             return res.data
         }
@@ -25,13 +28,17 @@
     }
     export default {
         middleware: "authenticated",
+        components: {
+            loader
+        },
         data: () => ({
-                user: null
+            user: null,
+                loaded: false
         }),
         async created() {
             const res = await fetchUser(this.$route.params.username)
             this.user = res
-            console.log(this.user)
+            this.loaded = true
         }
     }
 </script>
