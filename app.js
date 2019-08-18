54b0f39d4cc6f4e61e2a5d63c8d496ab00c36ab5
Yahya EZ-ZAINABI <yez-zain@student.1337.ma>
2019-08-18 13:56:39 +0000

    increase bodyparser limit, add upload static

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const https = require('https')
 const express = require('express')
 const mongoose = require('mongoose')
 const passport = require('passport')
+const BodyParser = require('body-parser')
 const cors = require('cors')
 const ejs = require('ejs')
 const { readFileSync } = require('fs')
@@ -12,6 +13,7 @@ const app = express()
 const PORT = process.env.PORT || 80
 const SPORT = process.env.PORT || 443
 const indexPath = resolve(__dirname, 'client', 'dist')
+const uploadPath = resolve(__dirname, 'uploads')
 const options = {
 	key: readFileSync('./key.pem'),
 	cert: readFileSync('./certificate.pem')
@@ -23,8 +25,12 @@ app.set('view engine', 'ejs')
 
 app.use(cors())
 app.use(passport.initialize())
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(express.urlencoded({
+	parameterLimit: 100000,
+	limit: '50mb',
+	extended: true
+}))
+app.use(express.json({limit: '50mb'}))
 
 mongoose
 	.connect(process.env.MONGO_URI, {
@@ -38,6 +44,7 @@ app.use('/api/users', require('./routes/users'))
 app.use('/api/oauth', require('./routes/oauth'))
 app.use('/oauth', require('./routes/oauth_ret'))
 app.use(express.static(indexPath))
+app.use('/uploads', express.static(uploadPath))
 app.get(/.*/, (req, res) => res.sendFile(`${indexPath}/index.html`))
 
 app
