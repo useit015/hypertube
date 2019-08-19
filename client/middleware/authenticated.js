951ee200b0c0bfad27775d72038c3661364c7902
Oussama Nahiz <onahiz@e2r8p4.1337.ma>
2019-08-19 05:07:48 +0100

    chnaged auth middleware

--- client/middleware/authenticated.js
+++ client/middleware/authenticated.js
@@ -1,5 +1,26 @@
-export default ({ store, redirect }) => {
-    if (!store.state.authenticated) {
-        return redirect('/sign')
+import axios from 'axios'
+
+const checkToken = async store => {
+    try {
+        const token = localStorage.getItem('token')
+        const url = `https://hypertube.tk/api/users/isloggedin`
+        const headers = { Authorization: `jwt ${token}` }
+        const res = await axios.get(url, { headers })
+        console.log('i called the server')
+        if (!res.data.err) {
+            store.dispatch('login', res.data)
+        }
+        return !res.data.err
+    } catch (err) {
+        console.log('Got error here -->', err)
+    }
+}
+
+export default async ({ store, redirect }) => {
+    if (!store.getters.authenticated) {
+        const authenticated = await checkToken(store)
+        if (!authenticated) {
+            return redirect('/sign')
+        }
     }
 }
