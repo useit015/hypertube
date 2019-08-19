3b718eccba3eddddfd095ad1471d6179d3854399
Oussama Nahiz <onahiz@e2r8p4.1337.ma>
2019-08-19 05:03:07 +0100

    added loggedIn middleware

---
+++ client/middleware/loggedIn.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+
+const checkToken = async store => {
+    try {
+        const token = localStorage.getItem('token')
+        const url = `https://hypertube.tk/api/users/isloggedin`
+        const headers = { Authorization: `jwt ${token}` }
+        const res = await axios.get(url, { headers })
+        console.log('i called the server from sign')
+
+        if (!res.data.err) {
+            store.dispatch('login', res.data)
+        }
+        return !res.data.err
+    } catch (err) {
+        console.log('Got error here -->', err)
+    }
+}
+
+export default async ({ store, redirect }) => {
+    if (store.getters.authenticated) {
+        return redirect('/library')
+    }
+    const authenticated = await checkToken(store)
+    if (authenticated) {
+        return redirect('/library')
+    }
+}
